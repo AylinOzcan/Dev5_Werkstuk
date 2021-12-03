8b4c27fc16ac8aa974438ed5302a2f1f4681b4e1
Aylin Özcan <aylin.ozcan@student.ehb.be>
2021-12-03 12:27:09 +0100

    refactor: add second endpoint

--- Werkstuk/api/src/server.js
+++ Werkstuk/api/src/server.js
@@ -25,6 +25,19 @@ const clients = [{
   res.send(`Hello world`)
 })
 
+/**
+ * [GET] /client by id
+ * @returns {Object} client from list clients
+ */
+ app.get(`/client/:id`, (req, res) => {
+  let id = req.query.id;
+
+  getClient(id);
+  console.log(client)
+
+  res.json(client);
+})
+
 app.listen(PORT, () => {
   console.log(`app listening at port ${PORT}`);
 });
