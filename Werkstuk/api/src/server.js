3d3082234a227c480db95a17ccc50d2b319d1f25
Aylin Özcan <aylin.ozcan@student.ehb.be>
2021-12-03 12:18:34 +0100

    refactor: cleaning server.js

--- Werkstuk/api/src/server.js
+++ Werkstuk/api/src/server.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000;
 
-/**
- * CRUD, 
- */
-
+/*
 const clients = [{
   id: 0,
   name: `Aylin Özcan`,
@@ -17,69 +14,12 @@ const clients = [{
   name: `Doctor Who`,
   email: `doctorw@tardis.com`
 }];
+*/
+
 
 /**
- * [GET] /
- * @returns {String} containing 'Hello world'
+ * CRUD, 
  */
-app.get(`/`, (req, res) => {
-  res.send(`Hello world`)
-})
-
-//app.use(`/api`);
-
-/**
- * [GET] /client by id
- * @returns {Object} client from list clients
- */
-app.get(`/client/:id`, (req, res) => {
-  let id = req.query.id;
-
-  getClient(id);
-  console.log(client)
-
-  res.json(client);
-})
-
-/**
- * [POST] /clients
- * @returns {Array} clients of objects
- */
-
-app.post(`/clients`, (req, res) => {
-  let clients = req.body;
-  res.send(clients)
-})
-
-/**
- * [DELETE] /client
- */
-app.delete('/client.:id', (req, res) => {
-  let id = req.query.id;
-  getClient(id);
-  //get client id and delete from list 
-})
-
-/**
- * [UPDATE] /client by id
- * @returns {Object} client with modifications
- */
-
-app.put('/client/:id', (req, res) => {
-  let id = req.query.id;
-  let update = req.body; //(? or param)
-  
-  getClient(id);
-  console.log(client)
-      
-  function updateClient (c){
-    c.name = update.name;
-    c.email = update.email;
-  }
-
-  updateClient(client);
-  res.json(client);
-})
 
 app.listen(PORT, () => {
   console.log(`app listening at port ${PORT}`);
@@ -90,6 +30,7 @@ app.listen(PORT, () => {
  * @param {*} id 
  * @returns client with correspended id
  */
+/*
 function getClient (id) {
   for(let client in clients){
     if(id == client[id]){
@@ -97,3 +38,4 @@ function getClient (id) {
     }
   }
 }
+*/
